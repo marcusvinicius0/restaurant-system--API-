b6df06c72cbede46b501cee47a2aa789d045e751
Marcus Vinicius <begheli@gmail.com>
2022-06-13 20:19:55 -0300

    restarting server

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface PayLoad {
+    sub: string;
+}
+
+export function isAuthenticated(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+
+    //receber o token
+    const authToken = req.headers.authorization;
+
+    if (!authToken) {
+        return res.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try {
+        //validar esse token.
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as PayLoad;
+
+        //to recover token id and put insade a variable user_id in req.    
+        req.user_id = sub;
+
+       return next();
+
+    } catch (err) {
+        return res.status(401).end();
+    }
+
+}
---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express, {Request, Response, NextFunction } from 'express';
+import path from 'path';
+
+import { router } from './routes';
+
+// const port = process.env.PORT || 3001
+
+
+const app = express();
+import cors from 'cors';
+
+app.use(express.json());
+app.use(cors()); //habilitando p/ qualquer IP/URL fazer requisição nessa API
+
+app.use(router);
+
+app.use(
+    '/files',
+    express.static(path.resolve(__dirname, '..', 'tmp'))
+)
+
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction)=> {
+
+    if(err instanceof Error){
+        //Se for uma instância do tipo error
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+})
+
+
+app.listen(process.env.PORT || 3000, () => console.log('server on'))
+
